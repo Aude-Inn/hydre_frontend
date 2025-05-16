1dc57cb3a64ffed325ae582b44b6927eb90098f0
Aude Martin <martin.aude@outlook.fr>
2025-05-16 15:09:57 +0200

    espace search b

--- src/pages/GameLists.tsx
+++ src/pages/GameLists.tsx
@@ -9,12 +9,12 @@ export function GameLists() {
         Game List :
       </h1>
 
-        <div className="bg-black/20 backdrop-blur-sm border border-teal-400/20 shadow-lg rounded-3xl p-4 sm:p-6">
-          <SearchBar />
-        </div>
+      <div className="w-full max-w-6xl space-y-16">
+
+      <div className="relative bg-black/20 backdrop-blur-sm border border-teal-400/20 shadow-lg rounded-3xl p-4 sm:p-6">
+  <SearchBar />
+</div>
      
-      <div className="w-full max-w-6xl space-y-8">
-       
         <div className="bg-black/20 backdrop-blur-sm border border-teal-400/20 shadow-lg rounded-3xl p-4 sm:p-8 overflow-x-auto">
           <GamesTable isDashboard={false} />
         </div>
