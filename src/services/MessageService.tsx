ee2469ebf9e6d1ed6935588f130e0b8d403ebcf6
Aude Martin <martin.aude@outlook.fr>
2025-05-22 22:02:51 +0200

    delete mess

--- src/services/MessageService.tsx
+++ src/services/MessageService.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { MessageData } from "../types/socket.types"; 
+import { API_ENDPOINTS } from "../utils/apiConfig";
+// import { MessageData } from "../types/socket.types"; 
 
-const API_URL = "http://localhost:5000/messages"; 
+const BASE_URL = API_ENDPOINTS
 
 // delete
 export const deleteMessage = async (messageId: string): Promise<void> => {
   try {
-    const response = await axios.delete(`${API_URL}/${messageId}`);
+    const response = await axios.delete(BASE_URL.messageById(messageId));
     if (response.status === 200 || response.status === 204) {
       console.log(`Message ${messageId} supprimé avec succès`);
     } else {
@@ -19,12 +20,12 @@ export const deleteMessage = async (messageId: string): Promise<void> => {
 };
 
 // fetch des messages
-export const fetchMessages = async (): Promise<MessageData[]> => {
-  try {
-    const response = await axios.get(API_URL);
-    return response.data;
-  } catch (error) {
-    console.error("Erreur lors de la récupération des messages :", error);
-    throw error;
-  }
-};
+// export const fetchMessages = async (): Promise<MessageData[]> => {
+//   try {
+//     const response = await axios.get(API_ENDPOINTS.messages);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Erreur lors de la récupération des messages :", error);
+//     throw error;
+//   }
+// };
