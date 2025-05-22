e424314ee0df41fd44deb5f92ce98f3f106f5eca
Aude Martin <martin.aude@outlook.fr>
2025-05-22 21:09:03 +0200

    socket ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from "socket.io-client";
 import { GameNotificationData, MessageData } from "../types/socket.types";
 
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 interface ServerToClientEvents {
   game_notification: (data: GameNotificationData) => void;
@@ -15,8 +16,7 @@ interface ClientToServerEvents {
 
 export type { ServerToClientEvents, ClientToServerEvents };
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  import.meta.env.VITE_API_URL || "http://localhost:5000",
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_URL,
   {
     transports: ["websocket"],
     autoConnect: true,
